4d65042176e9b18f2482d648f5afb0cce055608f
Bhoomi Sengar <sengarbhumi0@gmail.com>
2025-01-06 14:23:52 +0530

    LinkedList

---
+++ Linkedlist/LinkedList.java
@@ -0,0 +1,69 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void removeDuplicates() {
+        Node current = head;
+
+        while (current != null) {
+            Node runner = current;
+            while (runner.next != null) {
+                if (runner.next.data == current.data) {
+                    runner.next = runner.next.next; 
+                } else {
+                    runner = runner.next; 
+                }
+            }
+            current = current.next;
+        }
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(10);
+        list.add(30);
+        list.add(40);
+        list.add(20);
+
+        System.out.println("Original List:");
+        list.display();
+
+        list.removeDuplicates();
+
+        System.out.println("List after removing duplicates:");
+        list.display();
+    }
+}
